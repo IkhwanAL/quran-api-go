9e945141a330750f837f9183bb41077958c21b34
Ikhwan A. Latif <ikhwanal235@gmail.com>
2026-03-05 15:33:40 +0700

    feat(validator): add unit test for ValidatorIDParam function
    test to make sure Validator return error value if the value is not match

---
+++ pkg/validator/param_test.go
@@ -0,0 +1,38 @@
+package validator
+
+import "testing"
+
+func TestIOParamValidator(t *testing.T) {
+
+	// Check if id is empty return error value
+	id := ""
+	id, err := ValidateIDParam(id)
+	if err == nil {
+		t.Logf("expect validation id param to return error if the id does not contain any")
+		t.Fail()
+	}
+
+	// Check, Validate Param must return error if id is not a number
+	id = "Get"
+	id, err = ValidateIDParam(id)
+	if err == nil {
+		t.Logf("expect validation id param to return error if the id is not number")
+		t.Fail()
+	}
+
+	// Check, Validate Param must return errror if id is negative value
+	id = "-2"
+	_, err = ValidateIDParam(id)
+	if err == nil {
+		t.Logf("expect validation id param to return error if the id is negative")
+		t.Fail()
+	}
+
+	// Check, Validate Param must not return error if id is not negative value
+	id = "1"
+	id, err = ValidateIDParam(id)
+	if err != nil {
+		t.Logf("expect validation id param to accept id value")
+		t.Fail()
+	}
+}
